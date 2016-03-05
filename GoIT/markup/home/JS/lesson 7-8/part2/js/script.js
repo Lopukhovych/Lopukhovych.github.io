f1218a9dd92f1edacca1abfc91547f80974a6fd6
Volodymyr <volodymyr.lopukhovychh@gmail.com>
2016-03-06 00:00:40 +0200

    second

---
+++ GoIT/markup/home/JS/lesson 7-8/part2/js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+	var $link = $('.wrapper a');
+	// console.log($link);
+	$link.append("<em></em>");
+
+	$link.hover(function() {
+		$(this).find("em").stop(true, true).animate({opacity: "show"}, "slow");
+		var hoverText = $(this).attr("title");
+	    $(this).find("em").text(hoverText);
+	}, function() {
+		$(this).find("em").stop(true, true).animate({opacity: "hide"}, "slow");
+	});
+
+		var $button = $(".button");
+		$button.on('click', function(){
+			$link.find('em').show();
+		});
+
+	
+});
+
+
+		// $(this).find("em").stop(true, true).animate({opacity: "show"}, "slow");
